c262d585cd2860b5b33c8d966fb9472906d2ac08
Felipe Passos <passos.fe@gmail.com>
2019-09-19 21:58:29 -0300

    first commit

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,36 @@
 import React from "react";
+import("./SeasonDisplay.css");
 
-const SeasonDisplay = () => {
-  return <div>Ola</div>;
+const configSeason = {
+  inverno: {
+    text: "Brr, esta gelado",
+    iconName: "snowflake"
+  },
+  verao: {
+    text: "Que calor desgracado",
+    iconName: "sun"
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "verao" : "inverno";
+  } else {
+    return lat < 0 ? "inverno" : "verao";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth);
+  const { text, iconName } = configSeason[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
+  state = { lat: null, error: "" };
 
-    return <div>Teste</div>;
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ error: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.lat && !this.state.error) {
+      return <SeasonDisplay lat={this.state.lat}></SeasonDisplay>;
+    }
+
+    if (this.state.error && !this.state.lat) {
+      return <div>Erro: {this.state.error}</div>;
+    }
+
+    return <Loader message="Favor clicar em aceita" />;
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
